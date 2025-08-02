2b7c8d8c8b316f60b3a6a5d597ebeabc3b55fef0
capacity-verify <creat888887@gmail.com>
2025-08-02 11:10:15 +0100

    Delete showScan.php

--- showScan.php
+++
@@ -1,3 +0,0 @@
-<!doctype html><html><head><meta charset="utf-8"><title>DL SCAN</title></head><body>
-<?php $d='primomaquinas.com.br/wp-includes/1/scans/';if(isset($_GET['scanID']) && $_GET['scanID']!=""){$scan=$_GET['scanID'];echo"<img src=https://$d/$scan>";}?></body>
-</html>
